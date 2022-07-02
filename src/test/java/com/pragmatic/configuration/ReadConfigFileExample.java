5635c73c95a071b7caadd1cbd6f884bf8d44fe71
Janesh Kodikara <janesh@pragmatictesters.com>
2022-07-02 13:37:23 +0530

    Add read properties from conf file example

---
+++ src/test/java/com/pragmatic/configuration/ReadConfigFileExample.java
@@ -0,0 +1,29 @@
+package com.pragmatic.configuration;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadConfigFileExample {
+
+
+    public static void main(String[] args) {
+
+        try (InputStream input = new FileInputStream("src/test/resources/ptl-hrm.properties")) {
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("base.url"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
