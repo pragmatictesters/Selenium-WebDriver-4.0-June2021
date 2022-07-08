1a2b5bd3d0c17f6453d0a55b5b8f3d9668cbbc48
Janesh Kodikara <janesh@pragmatictesters.com>
2022-07-08 17:49:49 +0530

    Add Log4j2 examples with log4j.xml configuration

--- src/test/java/com/pragmatic/hrm/tests/LoginTest.java
+++ src/test/java/com/pragmatic/hrm/tests/LoginTest.java
@@ -3,15 +3,25 @@ package com.pragmatic.hrm.tests;
 import com.pragmatic.hrm.HRMBase;
 import com.pragmatic.hrm.pages.HRMLoginPage;
 import com.pragmatic.hrm.pages.MenuPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest  extends HRMBase {
 
 
+    private Logger logger = LogManager.getLogger();
 
     @Test
     public void testValidUserLogin(){
+        logger.trace("I am from Login test ");
+        logger.debug("I am from Login test ");
+        logger.info("I am from Login test ");
+        logger.warn("I am from Login test ");
+        logger.error("I am from Login test ");
+        logger.fatal("I am from Login test ");
+
         HRMLoginPage loginPage = new HRMLoginPage(driver);
         driver.get("http://hrm.pragmatictestlabs.com");
         loginPage.typeUsername("Admin").typePassword("Ptl@#321").clickLogin();
